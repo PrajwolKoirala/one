b505d1fb7b939f25f69ac1da15b9a6de4e8b944f
PrajwolKoirala <prajilakoirala124@gmail.com>
2022-09-21 17:44:53 +0545

    hello

---
+++ JS/variable.js
@@ -0,0 +1,11 @@
+var name
+name = "john"
+console.log("hello world" + name)
+
+para = "his name is john"
+ let age = 13.33
+is_married = true
+console.log(typeof(para, age, is_married))
+
+console.log({age})
+console.log(typeof(age))
---
+++ try_catch.js
@@ -0,0 +1,9 @@
+// b = 2
+// c = 3
+try{
+    let a = b+c
+    console.log({a})
+}
+catch(err) {
+    console.log(err.message);
+}
